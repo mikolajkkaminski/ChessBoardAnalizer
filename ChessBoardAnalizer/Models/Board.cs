c2ee411a7e7ca6e0b7f3173c3107ae774d7c30d2
mikolajkkaminski <mikolajkkaminski@gmail.com>
2023-02-14 15:55:31 +0100

    Moved logic of square board creation to board model

--- ChessBoardAnalizer/Models/Board.cs
+++ ChessBoardAnalizer/Models/Board.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 namespace ChessBoardAnalizer.Models
 {
     public class Board
     {
-        public List<Piece> pieces = new();
         public Board()
         {
             Piece queen = new Piece(PieceType.Queen, 0, 0);
             pieces.Add(queen);
+            AddSquaresToBoard();
         }
 
+        public List<Piece> pieces { get; set; } = new();
+        public Square[,] squares { get; set; } = new Square[8, 8];
+
+        private void AddSquaresToBoard()
+        {
+            int file = 0;
+            int rank = 0;
+
+            while (file <= 7 && rank <= 7)
+            {
+                squares[file, rank] = new();
+                squares[file, rank].WhiteOrBlack = 
+                    (file % 2 == 0 && rank % 2 == 0) ||
+                    (file % 2 == 1 && rank % 2 == 1)
+                    ? true : false;
+
+                if (file == 7)
+                {
+                    file = 0;
+                    rank += 1;
+                }
+                else
+                {
+                    file++;
+                }
+            }
+        }
+    }
+
+    public class Square
+    {
+        public bool WhiteOrBlack { get; set; }
+        public Piece? Occupation { get; set; }
     }
 }
